d03bc16ef45174eb486ef50c9917bd7e72262ba1
david.lee <david.lee@mindcurv.com>
2025-10-22 16:02:05 +0200

    add example for Solana transfer instruction

--- programs/solana-instructions/src/main.rs
+++ programs/solana-instructions/src/main.rs
@@ -1,3 +1,83 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use solana_client::nonblocking::rpc_client::RpcClient;
+use solana_commitment_config::CommitmentConfig;
+use solana_sdk::native_token::LAMPORTS_PER_SOL;
+use solana_sdk::signature::{Keypair, Signer};
+use solana_sdk::transaction::Transaction;
+use solana_system_interface::instruction::transfer;
+
+
+
+
+
+fn main() -> Result<()> {
+
+    example_transfer_instruction()
+
 }
+
+const RPC_URL_LOCAL:&str = "http://localhost:8899";
+
+#[tokio::main]
+async fn example_transfer_instruction() -> Result<()> {
+
+    //create a client connection
+    let client = RpcClient::new_with_commitment(RPC_URL_LOCAL.to_string(), CommitmentConfig::confirmed());
+
+    //generate keypair for sender and receiver
+    let sender = Keypair::new();
+    let receiver = Keypair::new();
+
+    //airdrop some SOL to the sender;
+    let airdrop_signature = client.request_airdrop(&sender.pubkey(), LAMPORTS_PER_SOL).await?;
+
+    loop {
+        let confirmation  = client.confirm_transaction(&airdrop_signature).await?;
+        if confirmation {
+            break;
+        }
+    }
+
+    println!("Airdrop succeeded");
+
+    // check balance before transfer
+    let sender_pre_balance = client.get_balance(&sender.pubkey()).await?;
+    let recipient_pre_balance = client.get_balance(&receiver.pubkey()).await?;
+
+    println!("Sender pre balance {}",sender_pre_balance);
+    println!("Receiver pre balance {}",recipient_pre_balance);
+
+    // define the amunt to send
+    let amount_to_send = LAMPORTS_PER_SOL / 100;
+
+    //Create an instruction
+    let transfer_instruction = transfer(&sender.pubkey(), &receiver.pubkey(), amount_to_send);
+
+    // add the instruction to a new transaction
+    let mut transaction = Transaction::new_with_payer(&[transfer_instruction],Some(&sender.pubkey()));
+    let blockhash = client.get_latest_blockhash().await?;
+
+    // this is why transaction is defined as being mutable
+    transaction.sign(&[&sender],blockhash);
+
+    //send the transaction on the network
+    match client.send_and_confirm_transaction(&transaction).await {
+        Ok(signature) => {
+            println!("transfer tx successful: {}", signature)
+        },
+        Err(error) => {
+            println!("{:?}", error)
+        }
+    }
+
+    let sender_post_balance = client.get_balance(&sender.pubkey()).await?;
+    let receiver_post_balance = client.get_balance(&receiver.pubkey()).await?;
+
+    println!("Sender post balance {}",sender_post_balance);
+    println!("Receiver post balance {}",receiver_post_balance);
+
+    Ok(())
+
+}
+
+
